0763ca3c339a2d9c644da0880cfa27de7465b582
Oleg Musienko <diogen737@gmail.com>
2021-12-20 17:19:06 +0100

    refactor: 💡 move universe rendering to the separate class

---
+++ www/components/universe-renderer.js
@@ -0,0 +1,166 @@
+import { Universe, Cell } from 'wasm-game-of-life';
+import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg.wasm';
+import { Fps } from './fps';
+
+
+export class UniverseRenderer {
+    constructor() {
+        this.fps = new Fps();
+        
+        // setup the universe 
+
+        const docStyles = getComputedStyle(document.body);
+        this.GRID_COLOR = docStyles.getPropertyValue('--theme-grid');
+        this.DEAD_COLOR = docStyles.getPropertyValue('--theme-bg');
+        this.ALIVE_COLOR = docStyles.getPropertyValue('--theme-cell-alive');;
+        this.CELL_SIZE = 7;
+
+        const canvasContainer = document.querySelector('.canvas-container');
+        this.uWidth = ~~(canvasContainer.clientWidth / (this.CELL_SIZE + 1));
+        // this.uWidth = 300;
+        this.uHeight = ~~(canvasContainer.clientHeight / (this.CELL_SIZE + 1));
+        // this.uHeight = 300;
+
+        this.canvas = document.getElementById('wasm-canvas');
+        this.canvas.height = (this.CELL_SIZE + 1) * this.uHeight + 1;
+        this.canvas.width = (this.CELL_SIZE + 1) * this.uWidth + 1;
+        this.ctx = this.canvas.getContext('2d');
+    }
+
+    jumpstart() {
+        this.universe = Universe.new(this.uWidth, this.uHeight);
+        // universe.spawn_glider(40, 40);
+        this.universe.set_random();
+        
+        this.drawGrid();
+        this.drawCells();
+
+        return this;
+    }
+
+    render() {
+        this.fps.render();
+        this.universe.tick();
+        this.drawCells();
+    }
+
+    tick() {
+        this.universe.tick();
+        this.drawCells();
+    }
+
+    set_all_dead() {
+        this.universe.set_all_dead();
+        this.drawCells();
+    }
+
+    set_random() {
+        this.universe.set_random();
+        this.drawCells();
+    }
+
+
+    handle_canvas_click(clickEvent, modifier) {
+        if (clickEvent.srcElement == this.canvas) {
+            console.log(clickEvent);
+
+            const boundingRect = this.canvas.getBoundingClientRect();
+            const scaleX = this.canvas.width / boundingRect.width;
+            const scaleY = this.canvas.height / boundingRect.height;
+            const canvasLeft = (clickEvent.clientX - boundingRect.left) * scaleX;
+            const canvasTop = (clickEvent.clientY - boundingRect.top) * scaleY;
+
+            const row = Math.min(Math.floor(canvasTop / (this.CELL_SIZE + 1)), this.uHeight - 1);
+            const col = Math.min(Math.floor(canvasLeft / (this.CELL_SIZE + 1)),this.uWidth - 1);
+
+            switch (modifier) {
+                case 'glider':
+                    this.universe.spawn_glider(row, col);
+                    break;
+                case 'pulsar':
+                    this.universe.spawn_pulsar(row, col);
+                    break;
+                default:
+                    this.universe.toggle_cell(row, col);
+            }
+
+            this.drawCells();
+        }
+    }
+
+    /**
+     * rendering logic
+     */
+    drawGrid() {
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = this.GRID_COLOR;
+
+        // vertical lines
+        for (let i = 0; i <= this.uWidth; ++i) {
+            const targetX = (this.CELL_SIZE + 1) * i + .5;
+            const targetY = (this.CELL_SIZE + 1) * this.uHeight + .5;
+            this.ctx.moveTo(targetX, 0);
+            this.ctx.lineTo(targetX, targetY);
+        }
+
+        // horizontal lines
+        for (let i = 0; i <= this.uHeight; ++i) {
+            const targetX = (this.CELL_SIZE + 1) * this.uWidth + .5;
+            const targetY = (this.CELL_SIZE + 1) * i + .5;
+            this.ctx.moveTo(0, targetY);
+            this.ctx.lineTo(targetX, targetY);
+        }
+
+        this.ctx.stroke();
+    }
+
+    drawCells() {
+        // get all cells for reference
+        const cellsPtr = this.universe.cells();
+        const cells = new Uint8Array(memory.buffer, cellsPtr, this.uHeight * this.uWidth);
+
+        // get cells diff from previous tick
+        const cellsDiffPtr = this.universe.cells_diff();
+        const cellsDiffLen = this.universe.cells_diff_len();
+        const cellsDiff = new Uint32Array(memory.buffer, cellsDiffPtr, cellsDiffLen);
+
+        // separate alive & dead cells for rendering optimization (fillStyle takes a lot of time)
+        const aliveCells = [];
+        const deadCells = [];
+        for (let idx of cellsDiff) {
+            const col = Math.floor(idx % this.uWidth);
+            const row = Math.floor(idx / this.uWidth);
+            if (cells[idx] === Cell.Alive) {
+                aliveCells.push([col, row]);
+            } else {
+                deadCells.push([col, row]);
+            }
+        }
+
+        this.ctx.beginPath();
+
+        // alive cells
+        this.ctx.fillStyle = this.ALIVE_COLOR;
+        for (let [col, row] of aliveCells) {
+            this.ctx.fillRect(
+                (this.CELL_SIZE + 1) * col + 1,
+                (this.CELL_SIZE + 1) * row + 1,
+                this.CELL_SIZE,
+                this.CELL_SIZE
+            );
+        }
+
+        // dead cells
+        this.ctx.fillStyle = this.DEAD_COLOR;
+        for (let [col, row] of deadCells) {
+            this.ctx.fillRect(
+                (this.CELL_SIZE + 1) * col + 1,
+                (this.CELL_SIZE + 1) * row + 1,
+                this.CELL_SIZE,
+                this.CELL_SIZE
+            );
+        }
+
+        this.ctx.stroke();
+    }
+}
--- www/index.js
+++ www/index.js
@@ -12,120 +12,14 @@ import 'bootstrap/js/dist/offcanvas';
  * - double tap on mobile for predefined counstructs
  */
 
-// game-of-life imports
+import { UniverseRenderer } from './components/universe-renderer';
 
-import { Universe, Cell } from 'wasm-game-of-life';
-import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg.wasm';
-import { Fps } from './components/fps';
-
-
-// setup the universe 
-
-const docStyles = getComputedStyle(document.body);
-
-const GRID_COLOR = docStyles.getPropertyValue('--theme-grid');
-const DEAD_COLOR = docStyles.getPropertyValue('--theme-bg');
-const ALIVE_COLOR = docStyles.getPropertyValue('--theme-cell-alive');;
-const CELL_SIZE = 7;
-
-const canvasContainer = document.querySelector('.canvas-container');
-
-const uWidth = ~~(canvasContainer.clientWidth / (CELL_SIZE + 1));
-// const uWidth = 300;
-const uHeight = ~~(canvasContainer.clientHeight / (CELL_SIZE + 1));
-// const uHeight = 300;
-
-const canvas = document.getElementById('wasm-canvas');
-const ctx = canvas.getContext('2d');
-canvas.height = (CELL_SIZE + 1) * uHeight + 1;
-canvas.width = (CELL_SIZE + 1) * uWidth + 1;
-
-
-// rendering logic
-
-const drawGrid = () => {
-    ctx.beginPath();
-    ctx.strokeStyle = GRID_COLOR;
-
-    // vertical lines
-    for (let i = 0; i <= uWidth; ++i) {
-        const targetX = (CELL_SIZE + 1) * i + .5;
-        const targetY = (CELL_SIZE + 1) * uHeight + .5;
-        ctx.moveTo(targetX, 0);
-        ctx.lineTo(targetX, targetY);
-    }
-
-    // horizontal lines
-    for (let i = 0; i <= uHeight; ++i) {
-        const targetX = (CELL_SIZE + 1) * uWidth + .5;
-        const targetY = (CELL_SIZE + 1) * i + .5;
-        ctx.moveTo(0, targetY);
-        ctx.lineTo(targetX, targetY);
-    }
-
-    ctx.stroke();
-}
-
-const drawCells = () => {
-    // get all cells for reference
-    const cellsPtr = universe.cells();
-    const cells = new Uint8Array(memory.buffer, cellsPtr, uHeight * uWidth);
-
-    // get cells diff from previous tick
-    const cellsDiffPtr = universe.cells_diff();
-    const cellsDiffLen = universe.cells_diff_len();
-    const cellsDiff = new Uint32Array(memory.buffer, cellsDiffPtr, cellsDiffLen);
-
-    // separate alive & dead cells for rendering optimization (fillStyle takes a lot of time)
-    const aliveCells = [];
-    const deadCells = [];
-    for (let idx of cellsDiff) {
-        const col = Math.floor(idx % uWidth);
-        const row = Math.floor(idx / uWidth);
-        if (cells[idx] === Cell.Alive) {
-            aliveCells.push([col, row]);
-        } else {
-            deadCells.push([col, row]);
-        }
-    }
-
-    ctx.beginPath();
-
-    // alive cells
-    ctx.fillStyle = ALIVE_COLOR;
-    for (let [col, row] of aliveCells) {
-        ctx.fillRect(
-            (CELL_SIZE + 1) * col + 1,
-            (CELL_SIZE + 1) * row + 1,
-            CELL_SIZE,
-            CELL_SIZE
-        );
-    }
-
-    // dead cells
-    ctx.fillStyle = DEAD_COLOR;
-    for (let [col, row] of deadCells) {
-        ctx.fillRect(
-            (CELL_SIZE + 1) * col + 1,
-            (CELL_SIZE + 1) * row + 1,
-            CELL_SIZE,
-            CELL_SIZE
-        );
-    }
-
-    ctx.stroke();
-}
-
-
-const fps = new Fps();
 
 // automatic ticks and rendering
 
 let frameId = null;
 const renderLoop = () => {
-    fps.render();
-    universe.tick();
-    drawCells();
+    renderer.render();
     frameId = requestAnimationFrame(renderLoop);
 }
 
@@ -164,11 +58,10 @@ pauseButton.addEventListener('click', () => {
 
 // manual single tick
 tickButton.addEventListener('click', () => {
-    universe.tick();
-    drawCells();
+    renderer.tick();
 });
 
-// cell toggle modifiers
+// cell toggles
 
 const modifiers = {
     Alt: {
@@ -192,54 +85,29 @@ document.addEventListener('keyup', e => {
 });
 
 // toggle a cell that the user has clicked on
-canvas.addEventListener('click', e => {
-    const boundingRect = canvas.getBoundingClientRect();
-    const scaleX = canvas.width / boundingRect.width;
-    const scaleY = canvas.height / boundingRect.height;
-
-    const canvasLeft = (e.clientX - boundingRect.left) * scaleX;
-    const canvasTop = (e.clientY - boundingRect.top) * scaleY;
-
-    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), uHeight - 1);
-    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), uWidth - 1);
-
-    // check keyboard modifiers
-    const patternSpawned = Object.values(modifiers).some(m => {
+document.addEventListener('click', e => {
+    let modifier;
+    for (let m of Object.values(modifiers)) {
         if (m.enabled) {
-            universe[`spawn_${m.method}`](row, col);
-            return true;
+            modifier = m.method;
         }
-    });
-
-    // if none were pressed do regular cell toggle
-    if (!patternSpawned) {
-        universe.toggle_cell(row, col);
     }
 
-    drawCells();
+    renderer.handle_canvas_click(e, modifier);
 });
 
 
 // reset all cells to a dead state
 resetDeadButton.addEventListener('click', () => {
-    // setting width resets all cells to a dead state
-    universe.set_all_dead();
-    drawCells();
+    renderer.set_all_dead();
 });
 
 // reset universe to a random state
 resetRandomButton.addEventListener('click', () => {
-    universe.set_random();
-    drawCells();
+    renderer.set_random();
 });
 
 
 // create and jumpstart the universe
-
-let universe = Universe.new(uWidth, uHeight);
-// universe.spawn_glider(40, 40);
-universe.set_random();
-
-drawGrid();
-drawCells();
+const renderer = new UniverseRenderer().jumpstart();
 play();
