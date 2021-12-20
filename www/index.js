c95cabfcb9c554bb3136c3d9f45bba30d9dbc0df
Oleg Musienko <diogen737@gmail.com>
2021-12-20 07:12:38 +0100

    refactor: 💡 move fps counter to a searate file

--- www/index.js
+++ www/index.js
@@ -2,6 +2,7 @@ import styles from './styles/index.css';
 
 import { Universe, Cell } from 'wasm-game-of-life';
 import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg.wasm';
+import { Fps } from './components/fps';
 
 
 // setup the universe 
@@ -102,46 +103,7 @@ const drawCells = () => {
 }
 
 
-const fps = new class {
-    constructor() {
-        this.fps = document.querySelector('.fps');
-        this.buffer = 2000;
-        this.frames = [];
-        this.lastFrameTimeStamp = performance.now();
-    }
-
-    render() {
-        const now = performance.now();
-        const delta = now - this.lastFrameTimeStamp;
-        this.lastFrameTimeStamp = now;
-        const fps = 1 / delta * 1000;
-
-        // save only last N readings
-        this.frames.push(fps);
-        if (this.frames.length > this.buffer) {
-            this.frames.shift();
-        }
-
-        // find min, max, avg fps
-        let min = Infinity;
-        let max = -Infinity;
-        let avg = 0;
-        for (let i = 0; i < this.frames.length; ++i) {
-            const frame = this.frames[i];
-            avg += frame;
-            min = Math.min(frame, min);
-            max = Math.max(frame, max);
-        }
-        avg = avg / this.frames.length;
-
-        this.fps.innerHTML = `
-FPS:<br>
-&nbsp;&nbsp;Min: ${min.toFixed(2)}<br>
-&nbsp;&nbsp;Max: ${max.toFixed(2)}<br>
-&nbsp;&nbsp;Avg: ${avg.toFixed(2)}`;
-    }
-}
-
+const fps = new Fps();
 
 // automatic ticks and rendering
 
